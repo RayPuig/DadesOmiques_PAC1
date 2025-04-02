74eb4d7a2f617ab6af3edae5c7c0aef51166092e
RayPuig <raypuig@gmail.com>
2025-04-03 00:14:43 +0200

    aact

--- codi/Render.R
+++ codi/Render.R
@@ -5,6 +5,6 @@ gc()
 
 #---------------------------- LECTURA -------------------------------------------------------------# 
 rmarkdown::render(input="./codi/PAC1_Rai_Puig_Treserra.Rmd",
-                  output_file=paste0(here::here("output/PEC1_Analisi_Dades_"),Sys.Date()),
+                  output_file=paste0(here::here("output/PEC1_Dades_Omiques_"),Sys.Date()),
                   params = list(dir_dades_desti=dir_dades_desti ,
                                 dir_dades_origen=dir_dades_origen))
